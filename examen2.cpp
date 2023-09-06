63ee94d9e3cb49735965b472fccc00eb2d21d506
datakrazh <141955790+datakrazh@users.noreply.github.com>
2023-09-06 15:49:18 -0600

    Parcial(contador).cpp

--- examen2.cpp
+++ examen2.cpp
@@ -1,29 +1,29 @@
 /*
 Instrucciones:
 	Subir 5 programas diferentes
-1er programa: a partir del número que das sumar 10 números
-2do programa: a partir del número que das sumar desde el 1 hasta el que das
-3er programa: a partir del número que das obtiene los impares hasta el número que diste
-4to programa: a partir del número que das obtiene su tabla de multiplicar
-5to programa: a partir del número que das suma los dígitos de ese número
-6to programa: a partir del número que das decir si es negativo y finalizar
-7mo programa: a partir del número que das hace un árbol con n pisos arriba y abajo
+1er programa: a partir del nÃºmero que das sumar 10 nÃºmeros
+2do programa: a partir del nÃºmero que das sumar desde el 1 hasta el que das
+3er programa: a partir del nÃºmero que das obtiene los impares hasta el nÃºmero que diste
+4to programa: a partir del nÃºmero que das obtiene su tabla de multiplicar
+5to programa: a partir del nÃºmero que das suma los dÃ­gitos de ese nÃºmero
+6to programa: a partir del nÃºmero que das decir si es negativo y finalizar
+7mo programa: a partir del nÃºmero que das hace un Ã¡rbol con n pisos arriba y abajo
 */
 
 #include<stdio.h>
 
 int main(){
 /*
-Se pone el valor escrito en "límite"
-Mientras el número (ahora 0) sea menor al límite impuesto:
-	Sumará +1 al 0
-	imprimirá salto de línea, el valor del número 0+1, salto de línea
+Se pone el valor escrito en "lÃ­mite"
+Mientras el nÃºmero (ahora 0) sea menor al lÃ­mite impuesto:
+	SumarÃ¡ +1 al 0
+	imprimirÃ¡ salto de lÃ­nea, el valor del nÃºmero 0+1, salto de lÃ­nea
 FIN
 */
 	int limite;
 	int numero=0;
 	
-	printf("hasta que numero quieres analizar los impares: ");
+	printf("hasta que numero quieres contar: ");
 	scanf("%d", &limite);
 	
 	while (numero<limite){
