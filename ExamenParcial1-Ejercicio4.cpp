884b6960c824c4e3e8b5e871089127c6d4bb861d
datakrazh <141955790+datakrazh@users.noreply.github.com>
2023-09-07 08:01:07 -0600

    Update ExamenParcial1-Ejercicio4.cpp

--- ExamenParcial1-Ejercicio4.cpp
+++ ExamenParcial1-Ejercicio4.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main() {
-     
+/*
+Se pedirá un número que se guardará en la int numero
+Se pedirá otro número que seguardará en la int lim
+La primer cantidad determinará el multiplicando y la segunda el multiplicador
+Mientras el incremento, que representa el aumento en el valor del multiplicador más adelante, sea menor que el límite que nosotros establecimos:
+     El incremento subirá su valor en 1
+     El resultado lo determinamos como el resultado de la multiplicación
+     Imprimiremos el resultado de la multiplicación en sentido de que se lee "número (multiplicando)" * "incremento (multiplicador)" = "resultado"
+Se imprimirá que terminó el programa (esto como guía para saber que funcionaba el while)
+*/
 int numero;
 int incremento=0;
 int resultado;
